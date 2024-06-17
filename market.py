438dad6434a7f7e417c9d3ee5663881a851327dd
simondvdv <simondv4@gmail.com>
2024-06-17 20:13:37 +0300

    17.06

--- market.py
+++ market.py
@@ -1,35 +1,61 @@
-def cash_receipt_maker_functional():
-    task = input('Введите команду:\n').lower()
-    match task:
-        case 'add':
-            shopping_cart_add()
-        case 'del':
-            print('del')
-        case 'view':
-            return price_list_view(price_dict)
-        case 'remove':
-            print('remove')
-        case 'cart':
-            pass
-        case _:
-            print('Такой команды нет')
-
-
 def price_list_view(price_list):
     print('\nСписок товаров:\n--------------------')
     counter = 0
     for key, value in price_list.items():
         counter += 1
         print(f'{counter}. {key} - {value}р.')
-    print('--------------------')
+        print('--------------------')
 
+
+def cart_list_view(price_list):
+    print('\nСписок товаров:\n--------------------')
+    counter = 0
+    for key, value in price_list.items():
+        counter += 1
+        print(f'{counter}. {key} - {value}р.')
+        print('--------------------')
 
 def shopping_cart_add():
-    pass
+    product = input('\nВведите название товара:\n').title()
+    return product
+
 
 def shop_request_check(shop_request):
     pass
 
 
-price_dict = {'Карандаш': 50, 'Ручка': 75.5, 'Пачка бумаги': 389.9, 'Линейка': 25.85}
+def did_i_nave_this(key, product):
+    if key in product.keys():
+        return 1
+    else:
+        return 0
 
+
+price_dict = {'Карандаш': 50, 'Ручка': 75.5, 'Пачка бумаги': 389.9, 'Линейка': 25.85}
+product_cart = {'Карандаш': 0, 'Ручка': 0, 'Пачка бумаги': 0, 'Линейка': 0}
+command = ''
+while command != 'end':
+
+    task = input('Введите команду:\n').lower()
+    match task:
+        case 'add':
+            while True:
+                try:
+                    key_ = shopping_cart_add()
+                    product_cart[key_] += did_i_nave_this(key_, product_cart)
+                    break
+                except KeyError:
+                    print('Такого товара нет, попробуйте ещё раз')
+                    continue
+        case 'del':
+            print('del')
+        case 'view':
+            price_list_view(price_dict)
+        case 'remove':
+            print('remove')
+        case 'cart':
+            cart_list_view(product_cart)
+        case 'end':
+            break
+        case _:
+            print('Такой команды нет')
